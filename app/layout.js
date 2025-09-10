93cca8377c586112cfbb015197ea982f2970b054
Walid329 <walidye329@gmail.com>
2025-09-09 19:45:40 -0500

    add seo

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,37 @@ import Footer from "../components/Footer"
 import AnimatedNavbar from "../components/AnimatedNavbar"
 import "./globals.css"
 
+
+export const metadata = {
+  title: "inkdgaf",
+  description: "music, tour dates, and shop from inkdgaf",
+  icons: {
+    icon: "/inkgaf.png", 
+  },
+  openGraph: {
+    title: "inkdgaf",
+    description: "music, tour dates, and shop from inkdgaf",
+    url: "https://inkdgaf.vercel.app/",
+    siteName: "inkdgaf",
+    images: [
+      {
+        url: "/og-image.png", 
+        width: 1200,
+        height: 630,
+        alt: "inkdgaf",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "inkdgaf",
+    description: "music, tour dates, and shop from inkdgaf",
+    images: ["/og-image.png"],
+  },
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
